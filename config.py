7225daeb20fe8526b4ff228092804ce2d52ce970
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-06 14:02:25 +0500

    add email variables

--- config.py
+++ config.py
@@ -4,10 +4,14 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     SECRET_KEY: str
     SQLALCHEMY_DATABASE_URI: str
+    EMAIL_USER: str
+    EMAIL_PASSWORD: str
+    EMAIL_HOST: str
+    EMAIL_PORT: int
 
     class Config:
         env_file = '.env'
 
 
-def get_settings() -> BaseSettings:
+def get_settings() -> Settings:
     return Settings()   # type: ignore
