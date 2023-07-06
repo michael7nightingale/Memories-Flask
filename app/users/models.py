6bb9b1aa72dd6bd5fe0782dbe0e7dcb8d548af21
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-06 14:02:45 +0500

    update activation logic

--- app/users/models.py
+++ app/users/models.py
@@ -4,11 +4,18 @@ from uuid import uuid4
 from app.extensions import db
 
 
-class User(db.Model, flask_login.UserMixin):    # type: ignore
+class UserMixin(flask_login.UserMixin):
+    @property
+    def is_active(self):
+        return self._is_active
+
+
+class User(db.Model, UserMixin):    # type: ignore
     id = db.Column(db.String(50), primary_key=True, default=lambda: str(uuid4()))
     username = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(200))
+    _is_active = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f"{self.id=}, {self.username=}"
--- app/users/views.py
+++ app/users/views.py
@@ -7,6 +7,8 @@ from flask_login import logout_user, login_user
 from app import schemas
 from .repository import UserRepository
 from app.extensions import login_manager, add_instance
+from .tokens import confirm_token, generate_token
+from app.emails import send_message
 
 
 @login_manager.user_loader
@@ -17,14 +19,25 @@ def load_user(user_id):
 blueprint = Blueprint("users", __name__, url_prefix='/auth', template_folder="templates", static_folder='static')
 
 
+def send_email_activation(user) -> None:
+    """ Generate and send activation link to email."""
+    token = generate_token(email=user.email)
+    link = request.host_url + blueprint.url_prefix + f"/{user.id}/" + token
+    message = f"""Finish registration by following the link:
+                {link}"""
+    send_message(
+        to_addrs=[user.email],
+        subject="User activation.",
+        body=message
+    )
+
+
 @blueprint.route("/login/", methods=['GET', 'POST'])
 def login():
     """Login user (by email)"""
     if request.method == "POST":
         try:
-            print(request.form.to_dict())
             user = UserRepository().login(request.form['email'], request.form['password'])
-            print(user)
             if user is not None:
                 login_user(user, remember=True, force=True)
                 return redirect(url_for('main.homepage'))
@@ -53,10 +66,9 @@ def register():
             user_schema = schemas.User(**request.form.to_dict())
             # password hashing
             user = UserRepository().register(**user_schema.dict())
-            add_instance(user)
-            flash("Вы успешно зарегистрированы", category='status')
-            os.makedirs(themes_blueprint.root_path + f'/static/images/{user.id}')
-            return redirect(url_for('users.login'))
+            send_email_activation(user)
+            flash("Activation link is sent to your email. Please, confirm you account.")
+            return redirect(url_for('main.homepage'))
         except Exception as e:
             print(e)
             flash("Ошибка при регистрации", category='status')
@@ -64,3 +76,18 @@ def register():
 
     # if GET request or validation is failed
     return render_template("register.html")
+
+
+@blueprint.get("/<uid>/<token>/")
+def activate_user(uid, token):
+    """Endpoint of activating user by following the link"""
+    user = UserRepository().get(uid)
+    email = confirm_token(token=token)
+    if email and user.email == email and not user._is_active:
+        user._is_active = True
+        add_instance(user)
+        os.makedirs(themes_blueprint.root_path + f'/static/images/{user.id}')
+        return redirect(url_for('users.login'))
+
+    return redirect(url_for("main.homepage"))
+
