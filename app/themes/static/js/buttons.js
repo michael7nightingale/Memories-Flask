2b4d8fdac1e6159c490758a191ffd86b20df27b8
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-13 13:26:09 +0500

    update templates and static

--- app/themes/static/js/buttons.js
+++ app/themes/static/js/buttons.js
@@ -2,15 +2,12 @@ function switchButton(event){
     let divId = event.currentTarget.value;
 
     let columns = document.getElementsByClassName("col search");
+    console.log(columns);
     for (let column in columns){
+        console.log(column);
         column.style.display = "none";
     }
     let targetColumn = document.getElementById(divId);
-    console.log(divId);
-    targetColumn.style.display = "block"
+        targetColumn.style.display = "block"
 
-}
-
-for (let button in document.getElementsByClassName('btn')){
-    button.addEventListener("onclick", switchButton)
 }
